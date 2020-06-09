de4b0e9a1c238ca47fc31d4e6176b50bfd7e9573
PeterBeben <pdbcas@gmail.com>
2020-06-09 14:42:02 -0400

    Bounding boxes

---
+++ BoundBox.cpp
@@ -0,0 +1,6 @@
+#include "BoundBox.h"
+
+BoundBox::BoundBox()
+{
+
+}
---
+++ BoundBox.h
@@ -0,0 +1,11 @@
+#ifndef BOUNDBOX_H
+#define BOUNDBOX_H
+
+
+class BoundBox
+{
+public:
+	BoundBox();
+};
+
+#endif // BOUNDBOX_H
