2f713d8deb970cc0bbb2da9ef0a697ba6907c103
PeterBeben <pdbcas@gmail.com>
2020-08-09 16:46:04 -0400

    Add alignment.h

---
+++ alignment.h
@@ -0,0 +1,24 @@
+//-----------------------------------------------------------
+//     Copyright (C) 2019 Piotr (Peter) Beben <pdbcas@gmail.com>
+//     See LICENSE included.
+
+#ifndef ALIGNMENT_H
+#define ALIGNMENT_H
+
+#include <Eigen/Dense>
+
+
+#define ALIGNED_MEMORY
+
+
+#ifdef ALIGNED_MEMORY
+const size_t ALIGNEDX = Eigen::Aligned16;
+inline size_t align_padded(size_t n) {
+	return ALIGNEDX > 0 ? ALIGNEDX*(1+((n-1)/ALIGNEDX)) : n;
+}
+#else
+const size_t ALIGNEDX = Eigen::Unaligned;
+inline size_t align_padded(size_t n) { return n; }
+#endif
+
+#endif // ALIGNMENT_H
